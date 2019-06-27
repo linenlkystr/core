7efe96fe4c2a391326b5c71d36f0f223681e5e6f
Steven A White <sawhite@ara.com>
2019-06-27 16:30:05 -0400

    Fixed a missing include header

--- projects/biogears/libBiogears/include/biogears/cdm/compartment/SECompartmentGraph.h
+++ projects/biogears/libBiogears/include/biogears/cdm/compartment/SECompartmentGraph.h
@@ -14,6 +14,8 @@ specific language governing permissions and limitations under the License.
 #include <string>
 #include <vector>
 
+#include <biogears/cdm/utils/Logger.h>
+
 namespace biogears {
 
 class Logger;
